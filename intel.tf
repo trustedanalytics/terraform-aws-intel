0af90e72c34bd903252a78c4d66ba0f0285d2b51
David Bishop <david@gnuconsulting.com>
2014-11-05 15:40:20 -0500

    initial commit

---
+++ intel.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+	region = "${var.aws_region}"
+}
+
+module "cf" {
+  source = "github.com/cloudfoundry-community/terraform-cf-aws-vpc"
+  #source = "../terraform-aws-vpc/"
+  network = "${var.network}"
+  aws_key_name = "${var.aws_key_name}"
+  aws_access_key = "${var.aws_access_key}"
+  aws_secret_key = "${var.aws_secret_key}"
+  aws_region = "${var.aws_region}"
+  aws_key_path = "${var.aws_key_path}"
+}
+
+module "cloudera" {
+  source = "github.com/teancom/terraform-aws-cloudera"
+  network = "${var.network}"
+  aws_centos_ami = "${lookup(var.aws_centos_ami, var.aws_region)}"
+  aws_key_name = "${var.aws_key_name}"
+  aws_vpc = "${module.cf.aws_default_vpc}"
+  aws_route_table_private = "${module.cf.private_route_table}"
+  aws_subnet_bastion = "${module.cf.bastion_subnet}"
+  hadoop_instance_count = "${var.hadoop_instance_count}" 
+  hadoop_instance_type = "${var.hadoop_instance_type}" 
+}
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_key_path" {}
+variable "aws_key_name" {}
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "network" {
+	default = "10.10"
+}
+
+variable "aws_centos_ami" {
+    default = {
+        us-east-1 = "ami-00a11e68"
+        us-west-1 = "ami-ba3c3bff"
+        us-west-2 = "ami-3425be04"
+        ap-northeast-1 = "ami-9392dc92"
+        ap-southeast-1 = "ami-dcbeed8e"
+        ap-southeast-2 = "ami-89e88db3"
+        eu-west-1 = "ami-af6faad8"
+        sa-east-1 = "ami-73ee416e"
+    }
+}
+
+variable "hadoop_instance_count" {
+        default = "5"
+}
+variable "hadoop_instance_type" {
+        default = "m3.2xlarge"
+}
