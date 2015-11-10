921d3b979e2a1f74857f7375465a20b4bd54496e
Sebastian Szwed <sebastian.szwed@intel.com>
2015-11-10 12:35:21 +0100

    changed submodules links to https

--- aws.tf
+++ aws.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "cloudera" {
 }
 
 module "consul" {
-  source = "git::git@github.com:trustedanalytics/terraform-aws-consul.git"
+  source = "github.com/trustedanalytics/terraform-aws-consul"
   aws_centos_ami = "${var.aws_centos_ami}"
   aws_key_name = "${var.aws_key_name}"
   aws_access_key = "${var.aws_access_key}"
