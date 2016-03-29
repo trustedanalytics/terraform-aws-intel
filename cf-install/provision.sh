66e4c706262bda2b28c700e1b899d5c67480b89c
Piotr Milewski <piotr.milewski@intel.com>
2016-03-29 13:48:58 +0200

    DPNG-6047 including rvm gpg key in repository

--- cf-install/provision.sh
+++ cf-install/provision.sh
@@ -111,7 +111,7 @@ esac
 if [[ ! -d "$HOME/.rvm" ]]; then
   cd $HOME
   gpg --list-keys
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  gpg --import /home/ubuntu/rvm.gpg
   curl -sSL https://get.rvm.io | bash -s stable
 fi
 
